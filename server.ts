dffe998ba053483f6abf5a1fa1cafb1062ac4b19
francanete <francanetedev@gmail.com>
2022-08-31 19:55:55 +0100

    Fix CORS error

--- server.ts
+++ server.ts
@@ -1,15 +1,30 @@
 require("dotenv").config();
 
 import express from "express";
+const cors = require("cors");
+
 import { Error } from "mongoose";
 const mongoose = require("mongoose");
 const notesRoutes = require("./routes/notes");
 
 const app = express();
 
+const whitelist = ["http://localhost:3000"];
+const corsOptions = {
+  origin: function (origin: any, callback: any) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+// app.use(cors(corsOptions));
+
 // Middleware
 
-app.use(express.json());
+app.use(express.json(), cors(corsOptions));
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
